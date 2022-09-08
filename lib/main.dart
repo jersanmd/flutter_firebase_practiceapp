f30fd8d67bb5aa938554a0efbf4543898a97a2f4
Jersan Balago <1stjersan@gmail.com>
2022-09-09 03:15:20 +0800

    CRUD Implemented Firebase Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -37,10 +38,146 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final CollectionReference _products = FirebaseFirestore.instance.collection('products');
 
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+
+  Future <void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if(documentSnapshot != null) {
+      _nameController.text = documentSnapshot['name'];
+      _priceController.text = documentSnapshot['price'].toString();
+    }
+
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext ctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(ctx).viewInsets.bottom+20
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                controller: _priceController,
+                decoration: const InputDecoration(
+                  labelText: 'Price'
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final String name = _nameController.text;
+                  final double? price = double.tryParse(_priceController.text);
+
+                  if(price != null) {
+                    await _products
+                      .doc(documentSnapshot!.id)
+                      .update({
+                        "name": name,
+                        "price": price
+                      });
+
+                      _nameController.text = '';
+                      _priceController.text = '';
+
+                      Navigator.of(context).pop();
+                  }
+                }, 
+                child: const Text('Update')
+              )
+            ],
+          ),
+        );
+      }
+    );
+  }
+
+  Future <void> _create() async {
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext ctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(ctx).viewInsets.bottom+20
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                controller: _priceController,
+                decoration: const InputDecoration(
+                  labelText: 'Price'
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final String name = _nameController.text;
+                  final double? price = double.tryParse(_priceController.text);
+
+                  if(price != null) {
+
+                    await _products.add({
+                      "name": name,
+                      "price": price
+                    });
+
+                      _nameController.text = '';
+                      _priceController.text = '';
+
+                      Navigator.of(context).pop();
+                  }
+                }, 
+                child: const Text('Add')
+              )
+            ],
+          ),
+        );
+      }
+    );
+  }
+
+  Future<void> _delete(String productId) async {
+    await _products.doc(productId).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('You have succesfully deleted a product.'))
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _create(),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: StreamBuilder(
         stream: _products.snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
@@ -54,6 +191,20 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: ListTile(
                     title: Text(documentSnapshot['name']),
                     subtitle: Text(documentSnapshot['price'].toString()),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () => _update(documentSnapshot), 
+                            icon: const Icon(Icons.edit)),
+                          
+                          IconButton(
+                            onPressed: () => _delete(documentSnapshot.id), 
+                            icon: const Icon(Icons.delete))
+                        ],
+                      ),
+                    )
                   )
                 );
               }
